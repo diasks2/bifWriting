7b4a58b7fb9dfc10c11d5b62384ab85e805dcfda
Kevin <diasks2@gmail.com>
2013-02-18 10:11:26 +0900

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BifWriting::Application.config.secret_token = '28602f69e3507630e8bb4d21d01ec91dcbf3cd30f6d804157c9932739f7453fafc0a138b5613c64708ad47881ed73c73aa13aef76e7b69831d6e372deb83e0f3'
